e23034eb3010999410fe2571b9f350079d3ddef8
chaitanya2810 <kchaitanya2895@gmail.com>
2018-06-20 12:34:50 -0500

    Initial Commit
    Initial Commit

--- app/src/main/java/com/example/chaitanyakulkarni/createaarforlogin/MainActivity.java
+++ app/src/main/java/com/example/chaitanyakulkarni/createaarforlogin/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.chaitanyakulkarni.createaarforlogin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.loginlibrary.APICalls;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
